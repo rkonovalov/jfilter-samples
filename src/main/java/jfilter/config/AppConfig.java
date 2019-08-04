80b681c9e8a19367af02ed89314f852efa8ed282
rkonovalov <rkonovalov86@gmail.com>
2019-08-04 10:55:27 -0400

    Added Spring boot config class

---
+++ src/main/java/jfilter/config/AppConfig.java
@@ -0,0 +1,13 @@
+package jfilter.config;
+
+import com.jfilter.EnableJsonFilter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+@EnableJsonFilter
+public class AppConfig {
+
+
+}
