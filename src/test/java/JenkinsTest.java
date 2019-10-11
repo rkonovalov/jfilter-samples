3113639cc651fbc61ee80cbbb6a1e4ec08679a63
rkonovalov <rkonovalov86@gmail.com>
2019-10-11 11:43:03 -0400

    Jenkins: removed bad test

--- src/test/java/JenkinsTest.java
+++ src/test/java/JenkinsTest.java
@@ -8,10 +8,4 @@ public class JenkinsTest {
     public void testJenkinsTrue() {
         Assert.assertEquals(5, 5);
     }
-
-    @Test
-    public void testJenkinsFalse() {
-        //For test Jenkins pipeline
-        Assert.assertEquals(5, 10);
-    }
 }
